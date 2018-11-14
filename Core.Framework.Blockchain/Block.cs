4fde63aef64f2867dbc23fbd30994a89119d0043
Rafael Cruzeiro <rafael.cruzeiro@neemu.com>
2018-11-14 16:40:44 -0200

    blockchain util tool

---
+++ Core.Framework.Blockchain/Block.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Core.Framework.Blockchain
+{
+    public sealed class Block
+    {
+        public int Index { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public int Nonce { get; set; }
+        public IList<object> Data { get; set; }
+
+        public Block(DateTimeOffset timestamp, object data)
+        {
+            Index = 0;
+            Timestamp = timestamp;
+            PreviousHash = null;
+            Nonce = 0;
+            Data = new List<object> { data };
+            Hash = CalculateHash();
+        }
+
+        public bool IsValid()
+        {
+            return Hash == CalculateHash();
+        }
+
+        public void CreateData(object data)
+        {
+            Data.Add(data);
+            Hash = CalculateHash();
+        }
+
+        internal string CalculateHash()
+        {
+            SHA256 sHA256 = SHA256.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(
+                $"{Timestamp}-{PreviousHash ?? ""}-{Nonce}-{JsonConvert.SerializeObject(Data)}");
+            byte[] outputBytes = sHA256.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        internal void Mine(int difficulty)
+        {
+            var leadingZeros = new string('0', difficulty);
+
+            while (Hash == null || Hash.Substring(0, difficulty) != leadingZeros)
+            {
+                Nonce++;
+                Hash = CalculateHash();
+            }
+        }
+    }
+}
---
+++ Core.Framework.Blockchain/Blockchain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Framework.Blockchain
+{
+    public sealed class Blockchain
+    {
+        public List<Block> Chain { get; private set; }
+
+        public Blockchain()
+        {
+            InitializeChain();
+        }
+
+        void InitializeChain()
+        {
+            Chain = new List<Block>
+            {
+                new Block(DateTimeOffset.Now, "{}")
+            };
+        }
+
+        public Block GetLatest()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public Block GetBlock(Func<Block, bool> predicate)
+        {
+            return Chain.SingleOrDefault(predicate);
+        }
+
+        public bool IsValid()
+        {
+            if (Chain.Count > 1)
+            {
+                for (int i = 1; i < Chain.Count; i++)
+                {
+                    Block currBlock = Chain[i];
+                    Block prevBlock = Chain[i - 1];
+
+                    if (currBlock.Hash != currBlock.CalculateHash())
+                        return false;
+
+                    if (currBlock.PreviousHash != prevBlock.Hash)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public Block AddBlock(Block block)
+        {
+            Block latestBlock = GetLatest();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Hash = block.CalculateHash();
+            Chain.Add(block);
+
+            return block;
+        }
+
+        public Block AddBlock(Block block, int difficulty)
+        {
+            Block latestBlock = GetLatest();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Mine(difficulty);
+            Chain.Add(block);
+
+            return block;
+        }
+    }
+}
