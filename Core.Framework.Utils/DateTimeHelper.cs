88953cece8b58462f6e908a7dbbe70de79a814b3
Rafael Cruzeiro <rafael.cruzeiro@gmail.com>
2019-04-15 12:16:55 -0300

    include new Utils project with DateTimeHelper util

---
+++ Core.Framework.Utils/DateTimeHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Framework.Utils
+{
+    public static class DateTimeHelper
+    {
+        public static bool IsBusinessDay(this DateTime date, List<DateTime> holidays = null)
+        {
+            if (date == default) throw new InvalidOperationException("Invalid date and time");
+
+            if (date.DayOfWeek == DayOfWeek.Saturday ||
+                date.DayOfWeek == DayOfWeek.Sunday ||
+                (holidays != null && holidays.Any() && holidays.Contains(date)))
+                return false;
+
+            return true;
+        }
+
+        public static DateTime GetNextBusinessDay(this DateTime date, List<DateTime> holidays = null)
+        {
+            if (date == default) throw new InvalidOperationException("Invalid date and time");
+
+            while (!IsBusinessDay(date, holidays))
+                date = date.AddDays(1.0);
+
+            return date;
+        }
+    }
+}
